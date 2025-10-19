e0862df87e2a9c085f8a368d01ab6417d8e57223
Harsh <harsh1912kanchan@gmail.com>
2025-10-19 18:23:08 +0530

    Initial commit

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleAuth from './GoogleAuth';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* Other login form or buttons */}
+      <GoogleAuth />
+    </div>
+  );
+};
+
+export default Login;
