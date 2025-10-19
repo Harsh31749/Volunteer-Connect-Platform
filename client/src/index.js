76a9498f6566acd8dd87be5bb37cc2592dd845e4
Harsh <harsh1912kanchan@gmail.com>
2025-10-19 18:23:08 +0530

    Initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import './index.css'
 
-const GOOGLE_CLIENT_ID = '998192214747-59q3n6bok95hlc9j6e0u20kdr22tvts9.apps.googleusercontent.com';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <React.StrictMode>
-        <Router> 
-            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}> 
-                <AuthProvider> 
-                    <App />
-                </AuthProvider>
-            </GoogleOAuthProvider>
-        </Router>
-    </React.StrictMode>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
 );
